0a91b6dd1fad39c9b7dfea2987fd010e49adf9d6
thorvaldsen <1s2t3i4a5n@gmail.com>
2021-04-02 10:38:13 +0200

    noko

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -1,30 +1,56 @@
 #include <iostream>
-
+#include <conio.h>
 
 int width = 20;
 int height = 20;
+int slange_pos [2] [50];//vertikale pos [1][X], horisontale pos [2][Y]
+int x, y, fruktx, frukty;
+eDirection dir;
+int score = 0;
 
+void Draw (int x, int y, int fruktx, int frukty);
 
 
 //initialiserer spelet
 void Setup (/* arguments */) {
-  /* code */
+  slange_pos [1][1] = 10;
+  slange_pos [2][1] = 10;
+  Draw(slange_pos[1][1],slange_pos[2][1],0, 0);
+
 }
 
 //Teikner kartet og slangen
-void Draw (/* arguments */) {
+void Draw (int x, int y, int fruktx, int frukty) {
 
-    //teikner øvre kant av boksen
-    for (int i = 0; i < width; i++) {
-      std::cout << "#";
+    for (int i = 0; i < height; i++) {
+      //teikner øvre og nedre kant av boksen
+      if ((i == 0) || (i == (height-1))) {
+        for (int j = 0; j < width; j++) {
+          std::cout << "#";
+        }
+        //teikner venstre og høgre kant
+      } else {
+        for (int j = 0; j < width; j++) {
+          if (j == 0 || j == (width-1)) {
+            std::cout << "#";
+          } else if (x == i && y == j) {
+              std::cout << "O";
+          } else {
+            std::cout << " ";
+
+            }
+          }
+        }
+
+      //resetter horisontaltelleren og hopper en rad nedover
+      int j = 0;
+      std::cout << "\n";
+
     }
-    //teikner v h veggane
-    for (int j = 0; j < height; j++) {
-      /* code */
-    }
+    std::cout << "Score: " << score;
 }
 
-//
+//sørger for bevegelse og sjekker kræsj, sender posisjonen til draw
 void Logic (/* arguments */) {
   /* code */
 }
@@ -36,8 +62,7 @@ void Input (/* arguments */) {
 
 //
 int main() {
-  Draw();
-
+    Setup();
 
   return 0;
 }
